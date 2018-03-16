8336e462ef5c4984d429863587d12511c6cc239f
m4jun <154417095@qq.com>
2018-03-16 21:15:24 +0800

    add scripts

---
+++ install-android-studio.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:maarten-fonville/android-studio
+
+sudo apt update
+
+sudo apt install android-studio
---
+++ install-mysql.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install mysql-server # install 
+mysql_secure_installation # config
+systemctl status mysql.service # boot service
+mysqladmin -p -u root version # cat version
+mysql -u root -p # login mysql
